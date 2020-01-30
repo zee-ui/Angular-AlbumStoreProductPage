57ae9a1e64b8ee945c9e4a6f8508399fc7b9b365
Zeeshan Shahab <mdshahab@microsoft.com>
2020-01-30 14:48:36 +0000

    Module Completed

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from "../product.service";
 })
 export class ProductListComponent implements OnInit {
 
-  private products: Product[];
+  products: Product[];
 
   constructor(private _productService: ProductService) {}
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productService = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -19,7 +19,7 @@ export class ProductService {
   }
 
   getProducts() :  Observable<Product[]> {
-    return this._http.get(this._productService).map((response) =>
+    return this._http.get(this._productsUrl).map((response) =>
     <Product[]>response.json());
   }
 
